6d756c8b214076621223883b4baa4e06c145f07a
pramadeish <111697757+pramadeish@users.noreply.github.com>
2023-04-23 11:20:01 +0530

    added questions screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 70, 11, 172),
-                Color.fromARGB(255, 126, 83, 202)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
+    const Quiz(),
   );
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+  final void Function() startQuiz;
   @override
   Widget build(context) {
     return Center(
@@ -11,7 +12,7 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             'assets/images/quiz-logo.png',
             width: 300,
-            color: Color.fromARGB(80, 247, 247, 247),
+            color: const Color.fromARGB(80, 247, 247, 247),
           ),
           const SizedBox(
             height: 100,
@@ -27,7 +28,7 @@ class StartScreen extends StatelessWidget {
             height: 40,
           ),
           OutlinedButton.icon(
-            onPressed: () {},
+            onPressed: startQuiz,
             style: OutlinedButton.styleFrom(
                 foregroundColor: Colors.white,
                 minimumSize: const Size(100, 50)),
